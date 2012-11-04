089c49ab9259b9e34362a7e7dc7153abd1eadae9
Benny Bach <benny.bach@gmail.com>
2012-11-04 11:53:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAgilityjsHelloWorld::Application.config.secret_token = 'cc2eec9eb10d6820cf17dd2f35abdb275e465e940b985cc810d4daf0b3f36bc4c1d8b1d26090e952af554389e566f934eef57cbd66f0c189e3242ab70d53f296'
